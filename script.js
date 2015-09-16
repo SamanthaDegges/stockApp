e56d0b17182f893e269a1877d56840df66e331be
berkeleybear <alicewu1992@gmail.com>
2015-09-15 23:11:45 -0700

    stock data can be read

--- script.js
+++ script.js
@@ -20,22 +20,28 @@ scotchApp.controller('mainController', function($scope) {
 });
 
 
-
-scotchApp.controller('addStocksController', function($scope){
+scotchApp.controller('addStocksController', function($scope, $http){
   $scope.message = "enter your stock symbol and click add to start tracking";
   var stockPortfolio = [];
   $scope.addStock = function() {
     stockPortfolio.push($scope.userInput);
     $scope.trackedStocks = stockPortfolio;
-    console.log($scope.trackedStocks);
+    $http.jsonp("http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol="+ $scope.userInput + "&callback=JSON_CALLBACK").
+    then(function(data) {
+      console.log(data.data.Change);
+      console.log("IT ACTUALLY WORKS"); 
+      return data
+    });
+    $scope.userInput = "";
   }
 });
 
 scotchApp.controller('trackedStocksController', function($scope){
   $scope.message = "Stocks you are currently tracking";
 });
-
-
-
-
-
+  //  $http.get("http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol="+ $scope.userInput + "&callback=myFunction").
+  //  then(function(data) {
+  //   console.log(data);
+  //   console.log("IT ACTUALLY WORKS");   
+  //   return data
+  // });
